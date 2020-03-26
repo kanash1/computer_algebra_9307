f7f9ef3f6c862659434f1745bfaad623380e51df
Samvel <41449047+sttie@users.noreply.github.com>
2020-03-26 17:41:02 +0300

    Update polynoms.py

--- polynoms.py
+++ polynoms.py
@@ -1,1 +1,18 @@
-.
+# Белов Игорь
+def DEG_P_N(polynom):
+    if len(polynom)==0:
+		return 0
+	for i in range(len(polynom) - 1, -1, -1):
+		if polynom[i]!=0:
+			return i
+	return 0
+
+# Белов Игорь
+def LED_P_Q(polynom):
+	if len(polynom)==0:
+		return 0; #zero polynom
+	else:
+		for i in range(len(polynom) - 1, -1, -1):
+			if polynom[i]!=0:
+				return polynom[i]
+	return 0; #also zero polynom
